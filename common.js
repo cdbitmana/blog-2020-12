a7ae0bf9a28666055f3def307102aa0ce57c511e
cdbitmana <cdbitmana@gmail.com>
2020-12-18 09:17:59 +0900

    20-12-18 배포

--- common.js
+++ common.js
@@ -2,8 +2,8 @@ function ArticleDetail__Body__init() {
 
     var body = document.querySelector('.article_body');
     var initialValue = body.innerHTML.trim();
-    initialValue = initialValue.replace("&gt;",">");
-    initialValue = initialValue.replace("&lt;","<");
+    initialValue = initialValue.replaceAll("&gt;",">");
+    initialValue = initialValue.replaceAll("&lt;","<");
 
     var viewer = new toastui.Editor.factory({
         
