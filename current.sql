2b44319b4a891a6fb02ab6e98f3d628c69faeda8
cdbitmana <cdbitmana@gmail.com>
2020-12-18 13:55:49 +0900

    배포

--- current.sql
+++ current.sql
@@ -35,8 +35,8 @@ CREATE TABLE `article` (
 /*Data for the table `article` */
 
 insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberId`,`boardId`,`hit`) values 
-(1,'2020-12-17 13:28:21','2020-12-17 13:28:21','자바기초 1강 변수','```java\r\nint a = 5;\r\nint b = 5;\r\nint c = a + b;\r\n```',1,3,0),
-(2,'2020-12-17 15:25:02','2020-12-17 15:25:59','자바기초 2강 조건문','```java\r\nint a = 5;\r\nif ( a < 10 ) {\r\nSystem.out.println(\"true\");\r\n} else if ( a > 10 ) {\r\nSystem.out.println(\"false\");\r\n}\r\n```',1,3,0);
+(1,'2020-12-17 13:28:21','2020-12-17 13:28:21','자바) Switch 구문','# SWITCH\r\n```java\r\nswitch (변수명) {\r\n          case 조건1:\r\n          실행코드1;\r\n\r\n          case 조건2:\r\n          실행코드2;\r\n\r\n          default:\r\n          실행코드3;\r\n}\r\n```\r\n\r\n변수가 \r\n조건1에 참이라면 실행코드1부터 실행코드3까지 순차적으로 실행\r\n조건2에 참이라면 실행코드2부터 실행코드3까지 순차적으로 실행\r\n조건 1과 조건2에 모두 맞지 않으면 실행코드3만 실행\r\n\r\n\r\n조건에 맞는 실행코드만 실행하고 싶다면 break; 를 넣어준다.\r\n# 예시\r\n```java\r\nint value = 1;\r\n\r\nswitch (value) {\r\n          case 1 :\r\n          System.out.println(\"1\");\r\n          break;\r\n\r\n          case 2 :\r\n          System.out.println(\"2\");\r\n          break;\r\n\r\n          case 3 :\r\n          System.out.println(\"3\");\r\n          break;\r\n\r\n         default :\r\n         System.out.println(\"그 외의 숫자\");\r\n}\r\n```\r\n',1,1,0),
+(2,'2020-12-17 15:25:02','2020-12-17 15:25:59','자바) 배열 정렬하는 식','```java\r\nint[] arr = new int[N];\r\n\r\n  for (int i = 0 ; i < arr.length ; i++) {\r\n    for (int j = 0 ; j < arr.length-i-1 ; j++) {\r\n      if(arr[j] < arr[j+1]){\r\n          int temp = arr[j+1];\r\n          arr[j+1] = arr[j];\r\n          arr[j] = temp;\r\n      }  \r\n    }\r\n  }\r\n```\r\n\r\n배열의 앞 뒤 값을  비교해서 작은 값을 뒤로 보내고, 다시 앞 뒤로 비교한다.\r\n이 과정을 반복해서 제일 작은 값은 마지막 위치로 가게 되고 제일 마지막 값은 이미 제일 작은 값이므로 또다시 비교할 필요가 없다.\r\n이 과정을 다시 반복하면 배열은 내림차순으로 정렬된다.',1,1,0);
 
 /*Table structure for table `articleRecommend` */
 
@@ -78,14 +78,12 @@ CREATE TABLE `board` (
   `name` char(20) COLLATE utf8_unicode_ci NOT NULL,
   `code` char(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `board` */
 
 insert  into `board`(`id`,`regDate`,`updateDate`,`name`,`code`) values 
-(1,'2020-12-17 13:20:51','2020-12-17 13:20:51','공지사항','notice'),
-(2,'2020-12-17 13:20:51','2020-12-17 13:20:51','자유','free'),
-(3,'2020-12-17 13:29:30','2020-12-17 13:29:30','IT','it');
+(1,'2020-12-18 10:17:45','2020-12-18 10:17:45','IT','it');
 
 /*Table structure for table `member` */
 
