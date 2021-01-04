35e88805ab5882c105dae96cf48aa15980e14025
cdbitmana <cdbitmana@gmail.com>
2021-01-04 13:54:36 +0900

    21-01-04 배포

--- common.js
+++ common.js
@@ -1,5 +1,22 @@
 
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
 
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            data: [0, 10, 5, 2, 20, 30, 45],
+            backgroundColor:['red','orange','yellow','green','blue','navy','purple']
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 
 $(window).on('scroll',function(){
 if($(window).scrollTop() >= 25){
